b826751e621e7028fea2fc8acaa07d85a9188aa7
dikiy77 <dikiy_77@mail.ru>
2018-10-11 10:08:48 +0300

    zdkjghska

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -31,6 +31,7 @@ const routes: Routes = [
       {
         path: 'personal-area',
         component: PersonalAreaComponent,
+
         children: [
           {
             path: 'personal-information',
