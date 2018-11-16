898b1494940efe5b60c5f00e0054553202fbe624
mrvet <alexoovsyanikov@gmail.com>
2018-11-16 12:04:06 +0300

    Guard was added

---
+++ src/app/guards/auth-guard.guard.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router} from '@angular/router';
+import { Observable } from 'rxjs';
+import {AuthService} from '../services/user/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuardGuard implements CanActivate {
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ){}
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+
+    if ( !this.authService.isAuth() ){
+
+      this.router.navigateByUrl('/authorize');
+
+      return false;
+    }//if
+
+    return true;
+
+  }//canActivate
+}
