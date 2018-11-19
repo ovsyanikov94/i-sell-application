b567df3ce4e42de1bbaa6d2034b51dc5be0431ba
mrvet <alexoovsyanikov@gmail.com>
2018-11-19 09:42:04 +0300

    Guard was added

--- src/app/services/user/auth.service.ts
+++ src/app/services/user/auth.service.ts
@@ -1,14 +1,26 @@
 import { Injectable } from '@angular/core';
+import { User } from '../../models/user/User';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
 
   isAuth(): boolean{
     return true;
   }
+
+  async register( user: User ){
+
+      return this.http.post(
+
+      );
+
+  }//register
 
 }
