053c41eb0ea35df457d74807029f74f40924bd89
mrvet <alexoovsyanikov@gmail.com>
2018-11-30 11:52:41 +0300

    Merged with master

--- src/app/models/user/User.ts
+++ src/app/models/user/User.ts
@@ -1,82 +1,8 @@
 'use strict';
 
 export class User {
-  // get userOldPassword(): string {
-  //   return this._userOldPassword;
-  // }
-  //
-  // set userOldPassword(value: string) {
-  //   this._userOldPassword = value;
-  // }
-  // get userPhone(): string {
-  //   return this._userPhone;
-  // }
-  //
-  // set userPhone(value: string) {
-  //   this._userPhone = value;
-  // }
-  //
-  // get userLastname(): string {
-  //   return this._userLastname;
-  // }
-  //
-  // set userLastname(value: string) {
-  //   this._userLastname = value;
-  // }
-  //
-  // get userName(): string {
-  //   return this._userName;
-  // }
-  //
-  // set userName(value: string) {
-  //   this._userName = value;
-  // }
-  //
-  // get userEmail(): string {
-  //   return this._userEmail;
-  // }
-  //
-  // set userEmail(value: string) {
-  //   this._userEmail = value;
-  // }
-  //
-  // get userConfirmPassword(): string {
-  //   return this._userConfirmPassword;
-  // }
-  //
-  // set userConfirmPassword(value: string) {
-  //   this._userConfirmPassword = value;
-  // }
-  //
-  // get userPhoto(): string {
-  //   return this._userPhoto;
-  // }
-  //
-  // set userPhoto(value: string) {
-  //   this._userPhoto = value;
-  // }
-  // get userCountSum(): number {
-  //   return this._userCountSum;
-  // }
-  //
-  // set userCountSum(value: number) {
-  //   this._userCountSum = value;
-  // }
 
-<<<<<<< HEAD
-  get userID(): string {
-      return this._id;
-    }
-
-  set userID(value: string) {
-    this._id = value;
-  }
-
-
-  private _id: string;
-=======
   public _id: string;
->>>>>>> master
   public userLogin: string;
   public userPassword: string;
   public userOldPassword: string;
@@ -88,34 +14,8 @@ export class User {
   public userPhoto: string;
   public userCountSum: number;
 
-  // get userPassword(): string {
-  //   return this._userPassword;
-  // }
-  //
-  // set userPassword(value: string) {
-  //   this._userPassword = value;
-  // }
-  //
-  // get userLogin(): string {
-  //   return this._userLogin;
-  // }
-  //
-  // set userLogin(value: string) {
-  //   this._userLogin = value;
-  // }
-
   constructor(){
 
-    /*this._userLogin = 'Login1';
-    this._userPassword = '';
-    this._userConfirmPassword = '';
-    this._userEmail = '';
-    this._userLastname = 'Lastname';
-    this._userName = 'Name';
-    this._userPhone = '';
-    this._userCountSum = 0;
-    this._userPhoto = 'https://cdnimg.rg.ru/img/content/156/70/54/1_d_850.jpg';
-*/
   }
 
 }
